744814ac83fda739a7b9c963d9a532d8b2aa88fa
TaiNguyen <tai@useget.com>
2021-05-17 22:48:27 +0700

    show data

--- mdc_basic/lib/home.dart
+++ mdc_basic/lib/home.dart
@@ -1,11 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mdc_basic/model/product.dart';
+import 'package:mdc_basic/model/product_repository.dart';
 
 class HomePage extends StatelessWidget {
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products.isEmpty) {
+      return const [];
+    }
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: theme.textTheme.headline6,
+                    maxLines: 1,
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    formatter.format(product.price),
+                    style: theme.textTheme.subtitle2,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        title: Text('Shrine'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
     );
--- mdc_basic/lib/model/product.dart
+++ mdc_basic/lib/model/product.dart
@@ -21,7 +21,7 @@ class Product {
   });
 
   String get assetName => '$id-0.jpg';
-  String get assetPackage => 'shine_images';
+  String get assetPackage => 'shrine_images';
 
   @override
   String toString() => "$name (id=$id)";
