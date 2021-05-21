3d8f3b96b1c9d88e514f15fb80c69828389310c5
TaiNguyen <tai@useget.com>
2021-05-21 23:02:28 +0700

    regalur string

--- getx_demo/lib/i18n/st_en_us.dart
+++ getx_demo/lib/i18n/st_en_us.dart
@@ -3,7 +3,7 @@ const Map<String, String> en = {
   'remove': 'Remove',
   'setting': 'Setting',
   'shopping': 'Shopping',
-  'total_amount_dollar_amount': 'Total amount: \$ @amount',
+  'total_amount_dollar_amount': r'Total amount: $ @amount',
   'please_turn_on_network_connection_to_continue_using_this_app.':
       'Please turn on network connection to continue using this app.',
 };
--- getx_demo/lib/i18n/st_vi_vn.dart
+++ getx_demo/lib/i18n/st_vi_vn.dart
@@ -3,7 +3,7 @@ const Map<String, String> vi = {
   'remove': 'Xóa',
   'setting': 'Cài đặt',
   'shopping': 'Mua sắm',
-  'total_amount_dollar_amount': 'Tổng tiền: @amount \$',
+  'total_amount_dollar_amount': r'Tổng tiền: @amount $',
   'please_turn_on_network_connection_to_continue_using_this_app.':
       'Hãy mở kết nối internnet để tiếp tục dùng app.',
 };
